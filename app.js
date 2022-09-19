5b6960a834705b59f7d5c7c001f8f5ad1e621a30
Elliot Robertson <elliotrobertson53@gmail.com>
2022-09-19 21:06:10 +0100

    updated bodyparser

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express ();
 const files = require ('./auctions.json');
 const cors = require ('cors');
 const bodyParser = require ('body-parser');
-app.use (bodyParser);
+app.use (bodyParser.urlencoded ({extended: true}));
 
 app.use (cors ());
 

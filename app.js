dfe7150e1e3bc1b872c9cc44698357133d1ee391
Elliot Robertson <elliotrobertson53@gmail.com>
2022-09-20 16:56:02 +0100

    added api endpoint

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
-const {
-getAllItems
-} = require('./controllers/app.controller');
+const cors = require ('cors');
+const express = require ('express');
+const app = express ();
+const {getAllItems} = require ('./controllers/app.controller');
 
-app.use(cors());
-app.use(express.json());
+app.use (cors ());
+app.use (express.json ());
 
-app.get('/', getAllItems)
+app.get ('/api', getAllItems);
 
-
-app.use((err, req, res, next) => {
+app.use ((err, req, res, next) => {
   if (err.status) {
-    res.status(err.status).send({msg: err.msg});
+    res.status (err.status).send ({msg: err.msg});
   } else {
-    next(err);
+    next (err);
   }
 });
 
-app.use((err, req, res, nex) => {
-  res.status(400).send({msg: 'Bad request'});
+app.use ((err, req, res, nex) => {
+  res.status (400).send ({msg: 'Bad request'});
 });
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({msg: 'internal server error'});
+app.use ((err, req, res, next) => {
+  console.log (err);
+  res.status (500).send ({msg: 'internal server error'});
 });
 
 module.exports = app;
